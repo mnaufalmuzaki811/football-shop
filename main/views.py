f013d960a25772eaff66d59dd5331db4e6e7efa5
mnaufalmuzaki811 <m.naufal.muzaki811@gmail.com>
2025-09-10 07:00:04 +0700

    Tugas 2: Menambahkan nama aplikasi

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'app_name' : 'Football Shop',
+        'app_name' : 'NOPALZ SPORTY',
         'name': 'Muhammad Naufal Muzaki',
         'npm': '2406428794'
     }
